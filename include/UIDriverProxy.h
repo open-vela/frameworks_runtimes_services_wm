d0b45c7bdd746dea0840ae60300f58e34d389208
yanxiaowei <yanxiaowei@xiaomi.com>
2023-06-26 11:04:12 +0800

    feat(server): add interfaces related with UIDriverProxy
    Change-Id: I4b1b9c2be5ae90c88794c0d13e8808a542bb098d

---
+++ include/UIDriverProxy.h
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2023 Xiaomi Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "wm/BufferQueue.h"
+#include "wm/Rect.h"
+
+namespace os {
+namespace wm {
+
+class BaseWindow;
+
+class UIDriverProxy {
+public:
+    UIDriverProxy(std::shared_ptr<BaseWindow> win) : mBaseWindow(win) {}
+    virtual ~UIDriverProxy() = 0;
+
+    virtual bool initUIInstance() = 0;
+
+    // ui proxy response window
+    virtual void onInvalidate() = 0;
+    virtual void* onDequeueBuffer() = 0;
+    virtual int onQueueBuffer() = 0;
+    virtual void* onCancelBuffer() = 0;
+
+    virtual void* getRoot() = 0;
+
+    // window request ui proxy, update buffer data
+    virtual bool drawFrame(std::shared_ptr<BufferItem> bufItem);
+
+    void onRectCrop(Rect& rect) {
+        mRectCrop = rect;
+    }
+
+    std::weak_ptr<BaseWindow> getBaseWindow() {
+        return mBaseWindow;
+    }
+
+    std::shared_ptr<BufferItem> getBufferItem() {
+        return mBufferItem;
+    }
+
+private:
+    static Rect NullRect;
+    std::weak_ptr<BaseWindow> mBaseWindow;
+    std::shared_ptr<BufferItem> mBufferItem;
+    Rect mRectCrop;
+};
+
+} // namespace wm
+} // namespace os
