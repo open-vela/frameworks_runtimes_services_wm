e9bcdf31503250d3fb71f25f0ecd8e13f1524855
yanxiaowei <yanxiaowei@xiaomi.com>
2023-06-26 10:59:01 +0800

    feat(server): add interfaces related with SurfaceControl
    Change-Id: I9ec34fff5ee4d8d7ea0fb44765bfc1100dfe4246

--- include/wm/SurfaceControl.h
+++ include/wm/SurfaceControl.h
@@ -16,28 +16,89 @@
 
 #pragma once
 
+#include <binder/IBinder.h>
 #include <binder/Parcel.h>
 #include <binder/Parcelable.h>
 #include <binder/Status.h>
 #include <utils/RefBase.h>
 
+#include <unordered_map>
+
+#include "wm/BufferQueue.h"
+
 namespace os {
 namespace wm {
 
-using namespace android;
-using namespace android::base;
-using namespace android::binder;
-using namespace std;
+using android::IBinder;
+using android::Parcel;
+using android::Parcelable;
+using android::sp;
+using android::status_t;
 
 class SurfaceControl : public Parcelable {
 public:
     SurfaceControl();
     ~SurfaceControl();
 
+    SurfaceControl(const sp<IBinder>& token, const sp<IBinder>& handle, uint32_t width = 0,
+                   uint32_t height = 0, uint8_t format = 0);
+
     status_t writeToParcel(Parcel* out) const override;
     status_t readFromParcel(const Parcel* in) override;
 
+    bool isValid();
+
+    std::unordered_map<BufferKey, BufferId>& bufferIds() {
+        return mBufferIds;
+    }
+
+    void initBufferIds(std::vector<BufferId>& ids);
+
+    void setBufferQueue(const std::shared_ptr<BufferQueue>& bq) {
+        mBufferQueue = bq;
+    }
+
+    std::shared_ptr<BufferQueue> bufferQueue() {
+        return mBufferQueue;
+    }
+
+    sp<IBinder> getToken() {
+        return mToken;
+    }
+
+    sp<IBinder> getHandle() {
+        return mHandle;
+    }
+
+    uint32_t getWidth() {
+        return mWidth;
+    }
+
+    uint32_t getHeight() {
+        return mHeight;
+    }
+
+    int32_t getFormat() {
+        return mFormat;
+    }
+
+    static bool isSameSurface(const std::shared_ptr<SurfaceControl>& lhs,
+                              const std::shared_ptr<SurfaceControl>& rhs);
+
 private:
+    // IWindow as token
+    sp<IBinder> mToken;
+
+    // SurfaceControl unique id
+    sp<IBinder> mHandle;
+
+    uint32_t mWidth;
+    uint32_t mHeight;
+    uint32_t mFormat;
+
+    // key: server/client unique id
+    std::unordered_map<BufferKey, BufferId> mBufferIds;
+    std::shared_ptr<BufferQueue> mBufferQueue;
 };
 
 } // namespace wm
