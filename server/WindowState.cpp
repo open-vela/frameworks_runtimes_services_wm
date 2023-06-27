8d2df7e022dac054efc015a14bd5c6bfc7c482f0
lijianjun <lijianjun@xiaomi.com>
2023-06-27 12:05:20 +0800

    feat(server): define interfaces for WindowState
    Change-Id: Id7f6666f2e9e6dfe5354c125f26e4f9e4fdc88eb

--- server/WindowState.cpp
+++ server/WindowState.cpp
@@ -13,3 +13,50 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+#define LOG_TAG "WindowState"
+
+#include "WindowState.h"
+
+#include <mqueue.h>
+#include <sys/mman.h>
+#include <utils/RefBase.h>
+
+#include "wm/InputMessage.h"
+
+namespace os {
+namespace wm {
+
+WindowState::WindowState(WindowManagerService* service, const sp<IWindow>& window,
+                         WindowToken* token, const LayoutParams& params, int32_t visibility)
+      : mClient(window), mToken(token), mService(service), mInputChannel(nullptr) {
+    mAttrs = params;
+    mVisibility = visibility != 0 ? true : false;
+}
+
+std::shared_ptr<InputChannel> WindowState::createInputChannel(const std::string name) {
+    if (mInputChannel != nullptr) {
+        ALOGE("mInputChannel is existed,create failed");
+        return nullptr;
+    }
+    struct mq_attr mqstat;
+    int oflag = O_CREAT | O_RDWR | O_NONBLOCK;
+    mqd_t messageQueue = 0;
+
+    memset(&mqstat, 0, sizeof(mqstat));
+    mqstat.mq_maxmsg = MAX_MSG;
+    mqstat.mq_msgsize = sizeof(InputMessage);
+    mqstat.mq_flags = 0;
+
+    if (((mqd_t)-1) == (messageQueue = mq_open(name.c_str(), oflag, 0777, &mqstat))) {
+        ALOGI("mq_open doesn't return success ");
+        return nullptr;
+    }
+
+    mInputChannel = std::make_shared<InputChannel>();
+    mInputChannel->setEventFd(messageQueue);
+    return mInputChannel;
+}
+
+} // namespace wm
+} // namespace os
