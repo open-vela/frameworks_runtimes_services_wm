4713f011b499dfbda78ca32b5cee792912d2d811
lijianjun <lijianjun@xiaomi.com>
2023-06-29 11:26:04 +0800

    feat(server): implement relayout,updateWindowTokenVisibility
    Change-Id: I9ffea5b6a588e6629b0389c74c1742709bc2b065

--- server/WindowManagerService.cpp
+++ server/WindowManagerService.cpp
@@ -18,13 +18,34 @@
 
 #include "WindowManagerService.h"
 
+#include <binder/IPCThreadState.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
 #include <utils/Log.h>
 
 #include "WindowState.h"
 #include "WindowToken.h"
+#include "wm/SurfaceControl.h"
+
+static const std::string graphicsPath = "/data/graphics/";
 
 namespace os {
 namespace wm {
+
+static inline std::string getUniqueId() {
+    return std::to_string(std::rand());
+}
+
+static inline BufferId createBufferId(int32_t pid) {
+    std::string bufferPath = graphicsPath + std::to_string(pid) + "/bq/" + getUniqueId();
+    int32_t fd = shm_open(bufferPath.c_str(), O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+    if (fd == -1) {
+        ALOGE("Failed to create shared memory,%s", strerror(errno));
+    }
+    std::hash<std::string> hasher;
+    int32_t bufferKey = hasher(bufferPath);
+    return {bufferKey, fd};
+}
 
 WindowManagerService::WindowManagerService() {}
 WindowManagerService::~WindowManagerService() {}
@@ -62,8 +83,9 @@ Status WindowManagerService::addWindow(const sp<IWindow>& window, const LayoutPa
     winToken->addWindow(win);
 
     if (outInputChannel != nullptr) {
-        // TODO: need define unique name
-        std::shared_ptr<InputChannel> inputChannel = win->createInputChannel("/data/InputMessage");
+        int32_t pid = IPCThreadState::self()->getCallingPid();
+        std::string name = graphicsPath + std::to_string(pid) + "/event/" + getUniqueId();
+        std::shared_ptr<InputChannel> inputChannel = win->createInputChannel(name);
         outInputChannel->copyFrom(*inputChannel);
     }
 
@@ -80,8 +102,25 @@ Status WindowManagerService::relayout(const sp<IWindow>& window, const LayoutPar
                                       int32_t requestedWidth, int32_t requestedHeight,
                                       int32_t visibility, SurfaceControl* outSurfaceControl,
                                       int32_t* _aidl_return) {
-    // TODO
-    *_aidl_return = 0;
+    *_aidl_return = -1;
+    sp<IBinder> client = IInterface::asBinder(window);
+    WindowState* win;
+    auto it = mWindowMap.find(client);
+    if (it != mWindowMap.end()) {
+        ALOGI("find winstate in map");
+        win = it->second;
+    } else {
+        win = nullptr;
+        return Status::ok();
+    }
+
+    if (visibility) {
+        ALOGI("createSurfaceControl() called");
+        *_aidl_return = createSurfaceControl(outSurfaceControl, win);
+    } else {
+        // TODO:destorySurfaceControl
+    }
+
     return Status::ok();
 }
 
@@ -111,7 +150,10 @@ Status WindowManagerService::removeWindowToken(const sp<IBinder>& token, int32_t
 
 Status WindowManagerService::updateWindowTokenVisibility(const sp<IBinder>& token,
                                                          int32_t visibility) {
-    // TODO
+    auto it = mTokenMap.find(token);
+    if (it != mTokenMap.end()) {
+        it->second->setClientVisible(visibility);
+    }
     return Status::ok();
 }
 
@@ -123,6 +165,26 @@ Status WindowManagerService::applyTransaction(const vector<LayerState>& state) {
 Status WindowManagerService::requestVsync(const sp<IWindow>& window, VsyncRequest freq) {
     // TODO
     return Status::ok();
+}
+
+int32_t WindowManagerService::createSurfaceControl(SurfaceControl* outSurfaceControl,
+                                                   WindowState* win) {
+    int32_t result = 0;
+
+    // double buffer: Create shared memory
+    int32_t pid = IPCThreadState::self()->getCallingPid();
+
+    vector<BufferId> ids;
+    for (int i = 0; i < 2; i++) {
+        ids.push_back(createBufferId(pid));
+    }
+    std::shared_ptr<SurfaceControl> surfaceControl = win->createSurfaceControl(ids);
+
+    if (surfaceControl != nullptr) {
+        outSurfaceControl->copyFrom(*surfaceControl);
+    }
+
+    return result;
 }
 
 } // namespace wm
