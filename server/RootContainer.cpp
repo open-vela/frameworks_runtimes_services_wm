4837a0773d49af0b12411922f779534f3234f339
yanxiaowei <yanxiaowei@xiaomi.com>
2023-06-29 11:29:44 +0800

    feat(container): add class RootContainer and init LVGL for wms
    Change-Id: Ia41c1ffa8a7bba7f529f49e02443954c65f4da04

---
+++ server/RootContainer.cpp
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2023 Xiaomi Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "RootContainer"
+
+#include "RootContainer.h"
+
+#include <lv_porting/lv_porting.h>
+#include <lvgl/lvgl.h>
+#include <nuttx/video/fb.h>
+#include <utils/Log.h>
+
+namespace os {
+namespace wm {
+
+RootContainer::RootContainer() : mDisp(nullptr), mSyncMode(DSM_TIMER) {
+#ifdef CONFIG_FB_SYNC
+    mVsyncEvent = FBIO_WAITFORVSYNC;
+#endif
+    init();
+}
+
+RootContainer::~RootContainer() {
+    // TODO: lvgl deinit
+    mDisp = nullptr;
+}
+
+lv_disp_t* RootContainer::getRoot() {
+    return mDisp;
+}
+
+lv_obj_t* RootContainer::getDefLayer() {
+    return lv_disp_get_scr_act(mDisp);
+}
+
+lv_obj_t* RootContainer::getSysLayer() {
+    return lv_disp_get_layer_sys(mDisp);
+}
+
+lv_obj_t* RootContainer::getTopLayer() {
+    return lv_disp_get_layer_top(mDisp);
+}
+
+bool RootContainer::init() {
+    lv_init();
+
+    // TODO: custom init for porting
+    lv_porting_init();
+
+    mDisp = lv_disp_get_default();
+    return mDisp ? true : false;
+}
+
+int RootContainer::drawFrame() {
+    lv_refr_now(mDisp);
+    return 0;
+}
+
+int RootContainer::handleEvent(int fd, int events, void* data) {
+    RootContainer* container = static_cast<RootContainer*>(data);
+    if (events == container->mVsyncEvent) {
+        _lv_disp_refr_timer(NULL);
+        return 0;
+    }
+    return -1;
+}
+
+int RootContainer::getFdInfo(int* fd, int* events) {
+    if (mSyncMode == DSM_VSYNC) {
+        if (fd) {
+            *fd = -1; // TODO: update it
+        }
+        if (events) {
+            *events = mVsyncEvent;
+        }
+        return 0;
+    }
+    return -1;
+}
+
+} // namespace wm
+} // namespace os
---
+++ server/RootContainer.h
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2023 Xiaomi Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <lvgl/lvgl.h>
+
+namespace os {
+namespace wm {
+
+typedef enum {
+    DSM_TIMER = 0,
+    DSM_VSYNC = 1,
+} DispSyncMode;
+
+class RootContainer {
+public:
+    RootContainer();
+    ~RootContainer();
+
+    lv_disp_t* getRoot();
+    lv_obj_t* getDefLayer();
+    lv_obj_t* getSysLayer();
+    lv_obj_t* getTopLayer();
+
+    int drawFrame();
+    DispSyncMode getSyncMode() {
+        return mSyncMode;
+    }
+    int getFdInfo(int* fd, int* events);
+    static int handleEvent(int fd, int events, void* data);
+
+private:
+    bool init();
+
+    lv_disp_t* mDisp;
+    DispSyncMode mSyncMode;
+    int mVsyncEvent;
+};
+
+} // namespace wm
+} // namespace os
