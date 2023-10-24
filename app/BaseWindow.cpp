7718548d01ce4bbb77d9ec2707e284c1a0235fb1
lijianjun <lijianjun@xiaomi.com>
2023-10-24 20:27:39 +0800

    Fix bug:shmfs coredump
    Change-Id: Ibcac1f3e5d3fe8ee3d0b78dbceb838b650155f03

--- app/BaseWindow.cpp
+++ app/BaseWindow.cpp
@@ -177,8 +177,8 @@ void BaseWindow::setInputChannel(InputChannel* inputChannel) {
 void BaseWindow::setSurfaceControl(SurfaceControl* surfaceControl) {
     /*reset current buffer when surface changed*/
     mUIProxy->resetBuffer();
-    mSurfaceControl.reset(surfaceControl);
     if (surfaceControl != nullptr && surfaceControl->isValid()) {
+        mSurfaceControl.reset(surfaceControl);
         mUIProxy->updateResolution(surfaceControl->getWidth(), surfaceControl->getHeight());
     }
 
